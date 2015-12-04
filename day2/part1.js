bed92c5aafe99ade6b5866a0595fd50f17810e93
Greg Walker <michael.g.walker@erdc.dren.mil>
2015-12-04 13:37:14 -0600

    Day 2, part 1

---
+++ day2/part1.js
@@ -0,0 +1,14 @@
+"use strict";
+const dimensions = require("fs").readFileSync("./dimensions.txt", { encoding: "ascii" }).trim().split("\n");
+
+let totalArea = 0;
+for(let dimension of dimensions) {
+	let dims = dimension.split("x").map(x => Number(x)).sort((v1, v2) => v1 > v2);
+
+	let pkgArea = 2 * (dims[0] * dims[1] + dims[0] * dims[2] + dims[1] * dims[2]);
+	pkgArea += (dims[0] * dims[1]);
+
+	totalArea += pkgArea;
+}
+
+console.log(totalArea);
