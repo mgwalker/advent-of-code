e6e9b3fd3219e1478c234ced4fe7bccbb5b393c1
mgwalker <mgwalker@gmail.com>
2015-12-05 10:05:52 -0600

    Cleaned up just a bit

--- day4/main.js
+++ day4/main.js
@@ -1,21 +1,20 @@
 "use strict";
 
 const crypto = require("crypto");
-const input = "yzbqklnj";
+const prefix = "yzbqklnj";
 
 let startsWith = process.argv[2];
 if(!startsWith) {
 	startsWith = "00000";
 }
 
-let collissions = 0;
 let num = 1;
 do {
 	const hash = crypto.createHash("md5");
-	hash.update(input + num);
+	hash.update(prefix + num);
 	if(hash.digest("hex").startsWith(startsWith)) {
 		console.log(`First int that produces a hash starting with ${startsWith}: ${num}`);
-		collissions++;
+		break;
 	};
 	num++;
-} while(collissions < 1);
+} while(true);
