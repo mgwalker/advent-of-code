ab765ab6c0d84bc4145548403f29ef4f74ab0be8
Greg Walker <mgwalker@gmail.com>
2016-12-04 11:56:43 -0600

    Day 1, part 2

--- 2016/python/day1/puzzle.py
+++ 2016/python/day1/puzzle.py
@@ -2,16 +2,36 @@ with open('input') as input:
     directions = input.read()
 
 directions = [piece.strip() for piece in directions.split(',')]
+grid = { '0,0': 1 }
+position = [ 0, 0 ]
+
 facing = 0
 facingChange = { 'R': 1, 'L': -1 }
+positionDeltas = [
+    {'R': 1, 'L': -1}, # facing north
+    {'R': -1, 'L': 1}, # east
+    {'R': -1, 'L': 1}, # south
+    {'R': 1, 'L': -1}  # west
+]
+firstRevisit = False
 
-# north, east, south, west
-blocks = [ 0, 0, 0, 0 ]
+for i,step in enumerate(directions):
+    direction = positionDeltas[facing][step[0]]
+    magnitude = int(step[1:])
+    path = [ position[0], position[1] ]
 
-for step in directions:
+    if not firstRevisit:
+        for x in range(magnitude):
+            path[i%2] += direction
+            gridIndex = '{},{}'.format(path[0], path[1])
+            if not gridIndex in grid:
+                grid[gridIndex] = 0
+            grid[gridIndex] += 1
+            if grid[gridIndex] > 1:
+                firstRevisit = [path[0], path[1]]
+
+    position[i%2] += (direction * magnitude)
     facing = (facing + facingChange[step[0]]) % 4
-    blocks[facing] += int(step[1:])
 
-distance = abs(blocks[0] - blocks[2]) + abs(blocks[1] - blocks[3])
-
-print('Easter Bunny HQ is {} blocks away'.format(distance))
+print('[part 1] Easter Bunny HQ is {} blocks away'.format(sum(position)))
+print('[part 2] Easter Bunny HQ is {} blocks away'.format(sum(firstRevisit)))
