fb22d211e0749c2fbdb305234cffecac7e43003e
Greg Walker <mgwalker@gmail.com>
2016-12-08 20:22:36 -0600

    Day 7, part 1

---
+++ 2016/python/day7/part1.py
@@ -0,0 +1,34 @@
+import re
+with open('input') as input:
+    lines = input.read().split('\n')
+
+supportsTLS = []
+
+for line in lines:
+    if not line:
+        continue
+
+    in_brackets = False
+    is_good = -1
+    for i in range(0,len(line)-3):
+        if in_brackets and line[i] == ']':
+            in_brackets = False
+            continue
+        elif line[i] == '[':
+            in_brackets = True
+        elif line[i] != line[i+1]:
+            one = line[i:i+2]
+            two = line[i+2:i+4][::-1]
+            if one == two:
+                if in_brackets:
+                    is_good = 0
+                    break
+
+                if is_good != 0:
+                    is_good = 1
+
+    if is_good == 1:
+        supportsTLS += [line]
+    #break
+
+print('[part 1] {} IPs support TLS'.format(len(supportsTLS)))
