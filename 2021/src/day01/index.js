274314f6dd905927d77096effede9c0f74e4c867
Greg Walker <mgwalker@gmail.com>
2021-12-01 08:09:22 -0600

    day 1

--- 2021/src/day01/index.js
+++ 2021/src/day01/index.js
@@ -1,17 +1,22 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput) => rawInput;
+const parseInput = (rawInput) => rawInput.trim().split("\n").map(Number);
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput);
-
-  return;
+  return input.filter((v, i) => (i === 0 ? false : v > input[i - 1])).length;
 };
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput);
 
-  return;
+  const windows = [];
+  for (let i = 0; i < input.length - 2; i += 1) {
+    windows.push(input.slice(i, i + 3).reduce((sum, v) => sum + v, 0));
+  }
+
+  return windows.filter((v, i) => (i === 0 ? false : v > windows[i - 1]))
+    .length;
 };
 
 run({
