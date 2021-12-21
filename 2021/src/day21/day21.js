518b2d14e62bc9123764f7fa822ac21e6db206bb
Greg Walker <mgwalker@gmail.com>
2021-12-21 01:40:07 -0600

    cleanup + comments

--- 2021/src/day21/day21.js
+++ 2021/src/day21/day21.js
@@ -51,18 +51,20 @@ export const part1 = (raw) => {
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-
   const inc = (from, by) => ((from + by - 1) % 10) + 1;
 
   const games = new Map([]);
 
   const doABarrelRoll = (game, roll) => {
     let [rolls, p1, p2, s1, s2] = game;
+
+    // Player 1's rolls are 0, 1, and 2
     if (rolls < 3) {
       p1 = inc(p1, roll);
+
+      // If this is the player's last roll of their turn, then we can increment
+      // their score. Don't increment every time!
       if (rolls === 2) {
-        // If this is the player's 3rd roll, then we update their score.
         s1 += p1;
       }
     } else {
@@ -74,37 +76,55 @@ export const part2 = (raw) => {
     }
     rolls = (rolls + 1) % 6;
 
+    // After this roll, we have created a new universe, so return its associated
+    // game ID.
     return [rolls, p1, p2, s1, s2].join(",");
   };
 
+  // A game ID is the string (without spaces, of course):
+  //   roll, p1 position, p2 position, p1 score, p2 score
+  // It represents a distinct universe. Playing a universe takes it from its
+  // current state and plays it until someone wins it. Playing a universe means
+  // spawning more universes, so this thingy is recursive.
   const playTheGame = (gameId) => {
+    // If we've already established the score that results from this universe,
+    // we can stop and return that. No need to run it all again.
     if (games.has(gameId)) {
       return games.get(gameId);
     }
 
     const game = gameId.split(",").map(Number);
-    const [rolls, , , s1, s2] = game;
+    const [turn, , , s1, s2] = game;
 
-    // We keep track of each roll independently for simplicity's sake. Rolls 0,
-    // 1, and 2 are from player 1, so if the roll number is 4, it is the end of
-    // player 1's turn and we should evaluate whether they've won. Likewise for
-    // player 2, roll number 0 indicates that their turn has just ended. In
-    // either case, if the player won, return a "win" count.
-    if (rolls === 3 && s1 >= 21) {
+    // We keep track of each roll independently, since each one spawns a new
+    // "universe." This means rolls 0, 1, and 2 are by player 1, and rolls 3,
+    // 4, and 5 are by player 2. Thus, if we're about to run roll 3, we've just
+    // finished player 1's turn and should check if they've won. Likewise for
+    // player turn and roll 0. If we're here and the player won, it's the first
+    // time they've ever won on this universe, so return a score array.
+    if (turn === 3 && s1 >= 21) {
       return [1, 0];
-    } else if (rolls === 0 && s2 >= 21) {
+    } else if (turn === 0 && s2 >= 21) {
       return [0, 1];
     }
 
+    // Spawn off new universes for the three possible outcomes of this roll.
+    // Grab the scores that we get back from each universe.
     const [s1a, s2a] = playTheGame(doABarrelRoll(game, 1));
     const [s1b, s2b] = playTheGame(doABarrelRoll(game, 2));
     const [s1c, s2c] = playTheGame(doABarrelRoll(game, 3));
 
+    // The preceding three universes represent the three universes that are
+    // spawned from this one. The score of THIS universe, therefore, is the
+    // sum of the scores of those universes.
     games.set(gameId, [s1a + s1b + s1c, s2a + s2b + s2c]);
+
+    // Return our universe's score.
     return games.get(gameId);
   };
 
-  const wins = playTheGame(`0,${data[0].position},${data[1].position},0,0`);
+  const [player1, player2] = input(raw);
+  const wins = playTheGame(`0,${player1.position},${player2.position},0,0`);
 
   return Math.max(...wins);
 };
