0f9e7f5ee95f01087d370470a4dcb2ef7f2a3696
Greg Walker <mgwalker@gmail.com>
2021-12-29 15:54:03 -0600

    2017 day 1

--- 2017/src/day01/day01.js
+++ 2017/src/day01/day01.js
@@ -1,11 +1,25 @@
-const input = (raw) => {};
+const input = (raw) => raw.split("").map(Number);
 
 export const part1 = (raw) => {
   const data = input(raw);
-  return;
+  let sum = 0;
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i] === data[(i + 1) % data.length]) {
+      sum += data[i];
+    }
+  }
+  return sum;
 };
 
 export const part2 = (raw) => {
   const data = input(raw);
-  return;
+  const skip = data.length / 2;
+
+  let sum = 0;
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i] === data[(i + skip) % data.length]) {
+      sum += data[i];
+    }
+  }
+  return sum;
 };
