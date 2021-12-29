5336fac94a14bee9bb0de7bb06ea87eecedc94e1
Greg Walker <mgwalker@gmail.com>
2021-12-29 16:04:11 -0600

    2017 day 2

--- 2017/src/day02/day02.js
+++ 2017/src/day02/day02.js
@@ -1,11 +1,28 @@
-const input = (raw) => {};
+const input = (raw) =>
+  raw
+    .trim()
+    .split("\n")
+    .map((l) => l.replace(/\s/g, " ").split(" ").map(Number));
 
-export const part1 = (raw) => {
-  const data = input(raw);
-  return;
-};
+export const part1 = (raw) =>
+  input(raw).reduce((sum, row) => sum + Math.max(...row) - Math.min(...row), 0);
 
 export const part2 = (raw) => {
   const data = input(raw);
-  return;
+
+  const divisions = data.map((row) => {
+    for (let i = 0; i < row.length; i += 1) {
+      for (let j = i + 1; j < row.length; j += 1) {
+        if (row[i] % row[j] === 0) {
+          return row[i] / row[j];
+        }
+        if (row[j] % row[i] === 0) {
+          return row[j] / row[i];
+        }
+      }
+    }
+    return 0;
+  });
+
+  return divisions.reduce((sum, c) => sum + c, 0);
 };
