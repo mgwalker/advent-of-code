eeec1d4cbfca06fb15d01c64cc9f5eb9644af82f
Greg Walker <mgwalker@gmail.com>
2021-12-30 14:51:34 -0600

    2017 day 12

--- 2017/README.md
+++ 2017/README.md
@@ -22,7 +22,7 @@
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
-![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -139,16 +139,16 @@ Both parts: 10.132666ms
 
 ```
 Day 11
-Time part 1: 1.429ms
-Time part 2: 1.391ms
-Both parts: 2.820041ms
+Time part 1: 1.454ms
+Time part 2: 1.46ms
+Both parts: 2.913958ms
 ```
 
 ```
 Day 12
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.576ms
+Time part 2: 2.854ms
+Both parts: 5.42975ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 16/50
-Total time: 168.326ms
+Total stars: 18/50
+Total time: 173.849ms
 ```
 
 <!--/RESULTS-->
--- 2017/src/day12/day12.js
+++ 2017/src/day12/day12.js
@@ -1,11 +1,56 @@
-const input = (raw) => {};
+const input = (raw) => {
+  const programs = new Map();
+
+  raw
+    .trim()
+    .split("\n")
+    .map((program) => {
+      const [id, connections] = program.split(" <-> ");
+      const neighbors = connections.replace(/\s/g, "").split(",").map(Number);
+      return { id: +id, neighbors };
+    })
+    .forEach((program) => programs.set(program.id, program));
+
+  for (const program of programs.values()) {
+    program.neighbors = program.neighbors.map((id) => programs.get(id));
+  }
+
+  return programs;
+};
+
+const findGroup = (from, withID) => {
+  const visited = new Set([withID]);
+
+  const queue = from.get(withID).neighbors;
+  while (queue.length > 0) {
+    const program = queue.pop();
+    if (!visited.has(program.id)) {
+      visited.add(program.id);
+
+      queue.push(...program.neighbors);
+    }
+  }
+
+  return visited;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const allPrograms = input(raw);
+  return findGroup(allPrograms, 0).size;
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const ungroupedPrograms = input(raw);
+  const groups = [];
+
+  for (const id of ungroupedPrograms.keys()) {
+    const group = findGroup(ungroupedPrograms, id);
+    for (const gid of group) {
+      ungroupedPrograms.delete(gid);
+    }
+
+    groups.push(group);
+  }
+
+  return groups.length;
 };
