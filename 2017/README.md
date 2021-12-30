55e2440a3c7d73ed0fd1df87cb8ae304a74ebeb2
Greg Walker <mgwalker@gmail.com>
2021-12-29 21:16:11 -0600

    2017 day 9

--- 2017/README.md
+++ 2017/README.md
@@ -19,7 +19,7 @@
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
-![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
@@ -125,9 +125,9 @@ Both parts: 1.951501ms
 
 ```
 Day 09
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.973ms
+Time part 2: 1ms
+Both parts: 3.972584ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 151.401ms
+Total stars: 12/50
+Total time: 155.373ms
 ```
 
 <!--/RESULTS-->
--- 2017/src/day09/day09.js
+++ 2017/src/day09/day09.js
@@ -1,11 +1,79 @@
-const input = (raw) => {};
+const input = (raw) => raw;
+
+function* drops(stream) {
+  let i = 0;
+  while (i < stream.length) {
+    while (stream[i] === "!") {
+      i += 2;
+    }
+    if (i < stream.length) {
+      yield stream[i];
+    }
+    i += 1;
+  }
+}
+
+const getScore = (node) => {
+  if (node.children.length === 0) {
+    return node.score;
+  }
+
+  return (
+    node.score + node.children.reduce((sum, child) => sum + getScore(child), 0)
+  );
+};
+
+const getGarbage = (node) => {
+  if (node.children.length === 0) {
+    return node.garbage.length;
+  }
+  return (
+    node.garbage.length +
+    node.children.reduce((sum, child) => sum + getGarbage(child), 0)
+  );
+};
+
+const process = (stream) => {
+  const root = { score: 1, parent: null, children: [], garbage: [] };
+  let current = root;
+  let inGarbage = false;
+
+  for (const drop of drops(stream.substring(1, stream.length - 2))) {
+    if (drop === "{" && !inGarbage) {
+      const group = {
+        score: current.score + 1,
+        parent: current,
+        children: [],
+        garbage: [],
+      };
+      current.children.push(group);
+      current = group;
+    }
+    if (drop === "}" && !inGarbage) {
+      current = current.parent;
+    }
+    if (drop === "<" && !inGarbage) {
+      inGarbage = true;
+    } else if (drop === ">") {
+      inGarbage = false;
+    } else if (inGarbage) {
+      current.garbage.push(drop);
+    }
+  }
+
+  return root;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const stream = input(raw);
+  const root = process(stream);
+
+  return getScore(root);
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const stream = input(raw);
+  const root = process(stream);
+
+  return getGarbage(root);
 };
