434b82a514887ee4a5d89b841f20695916b7d1bc
Greg Walker <mgwalker@gmail.com>
2021-12-30 15:57:05 -0600

    2017 day 13

--- 2017/README.md
+++ 2017/README.md
@@ -23,7 +23,7 @@
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
-![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
@@ -153,9 +153,9 @@ Both parts: 5.42975ms
 
 ```
 Day 13
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.165ms
+Time part 2: 53900.148ms
+Both parts: 53900.312207999996ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 18/50
-Total time: 173.849ms
+Total stars: 20/50
+Total time: 54074.162ms
 ```
 
 <!--/RESULTS-->
--- 2017/src/day13/day13.js
+++ 2017/src/day13/day13.js
@@ -1,11 +1,37 @@
-const input = (raw) => {};
+const input = (raw) =>
+  raw
+    .trim()
+    .split("\n")
+    .map((v) => v.replace(/\s/g, "").split(":").map(Number))
+    .map(([d, r]) => ({ depth: d, range: r }));
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const layers = input(raw);
+
+  return layers
+    .filter(({ depth, range }) => {
+      const timeToZero = range + range - 2;
+      return depth % timeToZero === 0;
+    })
+    .map(({ depth, range }) => depth * range)
+    .reduce((sum, c) => sum + c, 0);
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const layers = input(raw);
+
+  // find a t where depth+t below is non-zero for every layer
+  // let found = false;
+  let t = 0;
+  while (
+    // eslint-disable-next-line no-loop-func
+    !layers.every(({ depth, range }) => {
+      const timeToZero = range + range - 2;
+      return (depth + t) % timeToZero !== 0;
+    })
+  ) {
+    t += 1;
+  }
+
+  return t;
 };
