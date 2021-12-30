de65e9f4abd90a2d3bc91f4460ca5940469cff62
Greg Walker <mgwalker@gmail.com>
2021-12-29 18:13:27 -0600

    2017 day 5

--- 2017/src/day05/day05.js
+++ 2017/src/day05/day05.js
@@ -1,11 +1,37 @@
-const input = (raw) => {};
+const input = (raw) => raw.trim().split("\n").map(Number);
 
 export const part1 = (raw) => {
   const data = input(raw);
-  return;
+
+  let ip = 0;
+  let steps = 0;
+
+  while (ip < data.length) {
+    const jmp = data[ip];
+    data[ip] += 1;
+    ip += jmp;
+    steps += 1;
+  }
+
+  return steps;
 };
 
 export const part2 = (raw) => {
   const data = input(raw);
-  return;
+
+  let ip = 0;
+  let steps = 0;
+
+  while (ip < data.length) {
+    const jmp = data[ip];
+    if (jmp >= 3) {
+      data[ip] -= 1;
+    } else {
+      data[ip] += 1;
+    }
+    ip += jmp;
+    steps += 1;
+  }
+
+  return steps;
 };
