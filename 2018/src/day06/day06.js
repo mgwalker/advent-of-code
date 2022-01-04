6b1ae24067fa403a71164a2948042b7d6addd1c3
Greg Walker <mgwalker@gmail.com>
2022-01-04 12:45:10 -0600

    2018, day 26 started

--- 2018/src/day06/day06.js
+++ 2018/src/day06/day06.js
@@ -1,8 +1,12 @@
-const input = (raw) => {};
+const input = (raw) =>
+  raw
+    .trim()
+    .split("\n")
+    .map((l) => l.split(", ").map(Number));
 
 export const part1 = (raw) => {
   const data = input(raw);
-  return;
+  return data.slice(0, 3);
 };
 
 export const part2 = (raw) => {
