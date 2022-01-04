d1390503729e1c68850bbaeb9685810b7eb2a3f4
Greg Walker <mgwalker@gmail.com>
2022-01-04 08:15:14 -0600

    2018 day 4

--- 2018/README.md
+++ 2018/README.md
@@ -14,7 +14,7 @@
 [![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -90,9 +90,9 @@ Both parts: 293.198125ms
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 3.88ms
+Time part 2: 2.922ms
+Both parts: 6.802041ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 6/50
-Total time: 331.765ms
+Total stars: 8/50
+Total time: 338.567ms
 ```
 
 <!--/RESULTS-->
--- 2018/src/day04/day04.js
+++ 2018/src/day04/day04.js
@@ -1,11 +1,117 @@
-const input = (raw) => {};
+import CountingSet from "./CountingSet.js";
+
+const input = (raw) => {
+  const dtRegex = /\[(\d{4})-(\d{2})-(\d{2}) (\d{2}):(\d{2})\] (.*)/;
+
+  return raw
+    .trim()
+    .split("\n")
+    .map((line) => {
+      const [, year, month, day, hour, minute, action] = line.match(dtRegex);
+      const ts = +[year, month, day, hour, minute].join("");
+
+      return {
+        ts,
+        year: +year,
+        month: +month,
+        day: +day,
+        hour: +hour,
+        minute: +minute,
+        action,
+      };
+    })
+    .sort(({ ts: a }, { ts: b }) => a - b);
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const log = input(raw);
+
+  const elves = new Map();
+  let elf = null;
+  let start = NaN;
+
+  for (const { action, minute } of log) {
+    if (action.startsWith("Guard")) {
+      elf = +action.match(/#(\d+) /)[1];
+      if (!elves.has(elf)) {
+        elves.set(elf, new CountingSet());
+      }
+    } else if (action === "wakes up") {
+      const timecard = elves.get(elf);
+      for (let m = start; m < minute; m += 1) {
+        timecard.add(m);
+      }
+
+      if (minute < start) {
+        throw new Error("assumption invalid");
+      }
+    } else {
+      start = minute;
+    }
+  }
+
+  const [sleepiestElf] = [...elves.entries()]
+    .map(([key, value]) => [
+      key,
+      [...value.values()].reduce((sum, c) => sum + c, 0),
+    ])
+    .sort(([, a], [, b]) => a - b)
+    .pop();
+
+  const minutes = elves.get(sleepiestElf);
+  let sleepiestMinute = -1;
+  let most = -1;
+  for (const [minute, count] of minutes) {
+    if (count > most) {
+      sleepiestMinute = minute;
+      most = count;
+    }
+  }
+
+  return sleepiestElf * sleepiestMinute;
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const log = input(raw);
+
+  const elves = new Map();
+  let elf = null;
+  let start = NaN;
+
+  for (const { action, minute } of log) {
+    if (action.startsWith("Guard")) {
+      elf = +action.match(/#(\d+) /)[1];
+      if (!elves.has(elf)) {
+        elves.set(elf, new CountingSet());
+      }
+    } else if (action === "wakes up") {
+      const timecard = elves.get(elf);
+      for (let m = start; m < minute; m += 1) {
+        timecard.add(m);
+      }
+
+      if (minute < start) {
+        throw new Error("assumption invalid");
+      }
+    } else {
+      start = minute;
+    }
+  }
+
+  const [sleepiestElf] = [...elves.entries()]
+    .map(([key, value]) => [key, Math.max(...[...value.values()])])
+    .sort(([, a], [, b]) => a - b)
+    .pop();
+
+  const minutes = elves.get(sleepiestElf);
+  let sleepiestMinute = -1;
+  let most = -1;
+  for (const [minute, count] of minutes) {
+    if (count > most) {
+      sleepiestMinute = minute;
+      most = count;
+    }
+  }
+
+  return sleepiestElf * sleepiestMinute;
 };
