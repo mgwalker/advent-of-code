fca1919408f7110d313f611eb353d975d00d64b9
Greg Walker <mgwalker@gmail.com>
2022-01-03 21:16:05 -0600

    2018 day 1

--- 2018/src/day01/day01.js
+++ 2018/src/day01/day01.js
@@ -1,11 +1,22 @@
-const input = (raw) => {};
+const input = (raw) => raw.trim().split("\n").map(Number);
 
-export const part1 = (raw) => {
-  const data = input(raw);
-  return;
-};
+export const part1 = (raw) => input(raw).reduce((sum, c) => sum + c, 0);
 
 export const part2 = (raw) => {
   const data = input(raw);
-  return;
+
+  const seen = new Set();
+
+  let frequency = 0;
+  let index = 0;
+  do {
+    frequency += data[index];
+    if (seen.has(frequency)) {
+      break;
+    }
+    seen.add(frequency);
+    index = (index + 1) % data.length;
+  } while (true);
+
+  return frequency;
 };
