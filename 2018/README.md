be1c48fa402d75be763721736d9ea9b4b3ed476f
Greg Walker <michael.walker@gsa.gov>
2022-01-05 17:09:40 -0600

    2018 day 12

--- 2018/README.md
+++ 2018/README.md
@@ -22,7 +22,7 @@
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
-![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
@@ -139,16 +139,16 @@ Both parts: 158.582584ms
 
 ```
 Day 11
-Time part 1: 14.725ms
-Time part 2: 412.561ms
-Both parts: 427.285875ms
+Time part 1: 14.515ms
+Time part 2: 950.162ms
+Both parts: 964.677583ms
 ```
 
 ```
 Day 12
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.251ms
+Time part 2: 13.136ms
+Both parts: 14.386750000000001ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 22/50
-Total time: 10955.169ms
+Total stars: 24/50
+Total time: 11506.948ms
 ```
 
 <!--/RESULTS-->
--- 2018/src/day12/day12.js
+++ 2018/src/day12/day12.js
@@ -1,11 +1,64 @@
-const input = (raw) => {};
+const input = (raw) => {
+  const [init, patt] = raw.split("\n\n");
+
+  const [, initial] = init.match(/initial state: (.*)$/);
+  const patterns = new Map(patt.split("\n").map((v) => v.split(" => ")));
+
+  return { initial, patterns };
+};
+
+const dots = [".", ".", ".", ".", "."];
+const cycle = (start, patterns) => {
+  const plants = [dots, start, dots].flat();
+  const out = [...plants];
+
+  for (let i = 2; i < plants.length - 2; i += 1) {
+    out[i] = patterns.get(plants.slice(i - 2, i + 3).join("")) ?? ".";
+  }
+
+  return out;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const { initial, patterns } = input(raw);
+
+  let plants = initial.split("");
+  let start = 0;
+
+  for (let i = 0; i < 20; i += 1) {
+    plants = cycle(plants, patterns);
+    start -= dots.length;
+  }
+
+  return plants
+    .map((v, i) => (v === "#" ? start + i : 0))
+    .reduce((sum, c) => sum + c, 0);
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const { initial, patterns } = input(raw);
+
+  let plants = initial.split("");
+  let start = 0;
+
+  const getSum = (plant, zero) =>
+    plants
+      .map((v, i) => (v === "#" ? zero + i : 0))
+      .reduce((sum, c) => sum + c, 0);
+
+  // By observation, the total sum begins to increase by a constant amount after
+  // around 100 iterations, so we don't actually need to run them all. Just run
+  // enough to start repeating.
+  let diff = 0;
+  let last = 0;
+  for (let i = 0; i < 100; i += 1) {
+    plants = cycle(plants, patterns);
+    start -= dots.length;
+
+    const sum = getSum(plants, start);
+    diff = sum - last;
+    last = sum;
+  }
+
+  return getSum(plants, start) + diff * (50_000_000_000 - 100);
 };
