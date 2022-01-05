2cae6519e5ba50082c9f49be5946b109a998f722
Greg Walker <michael.walker@gsa.gov>
2022-01-05 16:13:48 -0600

    2018 day 11

--- 2018/README.md
+++ 2018/README.md
@@ -20,8 +20,8 @@
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
-![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
+[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
@@ -132,16 +132,16 @@ Both parts: 498.739959ms
 
 ```
 Day 10
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 82.108ms
+Time part 2: 76.475ms
+Both parts: 158.582584ms
 ```
 
 ```
 Day 11
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 14.725ms
+Time part 2: 412.561ms
+Both parts: 427.285875ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 18/50
-Total time: 10369.301ms
+Total stars: 22/50
+Total time: 10955.169ms
 ```
 
 <!--/RESULTS-->
--- 2018/src/day11/day11.js
+++ 2018/src/day11/day11.js
@@ -1,11 +1,61 @@
-const input = (raw) => {};
+const input = (raw) => {
+  const serialNumber = +raw;
+  const grid = [...Array(301)].map((_, y) =>
+    [...Array(301)].map((_, x) => {
+      const rackID = x + 10;
+      const power = (rackID * y + serialNumber) * rackID;
+      return (Math.floor(power / 100) % 10) - 5;
+    })
+  );
+  return grid;
+};
+
+const getMax = (grid, size = 3) => {
+  let max = -Infinity;
+  const maxTopLeftCords = [-1, -1];
+
+  for (let y = 1; y <= 300 - size + 1; y += 1) {
+    for (let x = 1; x <= 300 - size + 1; x += 1) {
+      const coords = [];
+      for (let xo = 0; xo < size; xo += 1) {
+        for (let yo = 0; yo < size; yo += 1) {
+          coords.push([x + xo, y + yo]);
+        }
+      }
+
+      const power = coords.reduce((sum, [xx, yy]) => sum + grid[yy][xx], 0);
+
+      if (power > max) {
+        max = power;
+        maxTopLeftCords[0] = x;
+        maxTopLeftCords[1] = y;
+      }
+    }
+  }
+
+  return [max, maxTopLeftCords];
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const grid = input(raw);
+  return getMax(grid)[1].join(",");
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const grid = input(raw);
+
+  let max = -Infinity;
+  const coords = [-1, -1, -1];
+
+  for (let size = 1; size <= 15; size += 1) {
+    const [localMax, maxCoords] = getMax(grid, size);
+    if (localMax > max) {
+      max = localMax;
+      coords[0] = maxCoords[0];
+      coords[1] = maxCoords[1];
+      coords[2] = size;
+    }
+  }
+
+  return coords.join(",");
 };
