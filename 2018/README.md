ae98eb77299e12a90acba15de57f7650df109aa5
Greg Walker <mgwalker@gmail.com>
2022-01-04 20:12:29 -0600

    2018 day 7

--- 2018/README.md
+++ 2018/README.md
@@ -17,7 +17,7 @@
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
-[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%86/yellow)](src/day07)
+[![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
@@ -111,9 +111,9 @@ Both parts: 3522.1752500000002ms
 
 ```
 Day 07
-Time part 1: 0.448ms
-Time part 2: -
-Both parts: 0.448209ms
+Time part 1: 0.519ms
+Time part 2: 2.679ms
+Both parts: 3.197792ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 13/50
-Total time: 9865.043ms
+Total stars: 14/50
+Total time: 9867.793ms
 ```
 
 <!--/RESULTS-->
--- 2018/src/day07/day07.js
+++ 2018/src/day07/day07.js
@@ -9,6 +9,16 @@ const input = (raw) =>
       return [parent, child];
     });
 
+const sort = ({ id: a }, { id: b }) => {
+  if (a > b) {
+    return -1;
+  }
+  if (a < b) {
+    return 1;
+  }
+  return 0;
+};
+
 export const part1 = (raw) => {
   const data = input(raw);
 
@@ -30,23 +40,13 @@ export const part1 = (raw) => {
     .map(([, node]) => node);
   const order = [];
 
-  const sort = ({ id: a }, { id: b }) => {
-    if (a > b) {
-      return -1;
-    }
-    if (a < b) {
-      return 1;
-    }
-    return 0;
-  };
-
   while (nextSteps.length > 0) {
     nextSteps.sort(sort);
     const next = nextSteps.pop();
     order.push(next.id);
     next.children.forEach((node) => {
       node.parents.delete(next);
-      if (node.parents.size === 0 && !order.includes(node.id)) {
+      if (node.parents.size === 0) {
         nextSteps.push(node);
       }
     });
@@ -57,5 +57,63 @@ export const part1 = (raw) => {
 
 export const part2 = (raw) => {
   const data = input(raw);
-  return;
+
+  const nodes = new Map();
+
+  for (const [parent, child] of data) {
+    if (!nodes.has(parent)) {
+      nodes.set(parent, { id: parent, parents: new Set(), children: [] });
+    }
+    if (!nodes.has(child)) {
+      nodes.set(child, { id: child, parents: new Set(), children: [] });
+    }
+    nodes.get(parent).children.push(nodes.get(child));
+    nodes.get(child).parents.add(nodes.get(parent));
+  }
+
+  const time = new Map(
+    [...Array(26)].map((_, i) => [String.fromCharCode(i + 65), i + 61])
+  );
+
+  let elves = 5;
+  let t = 0;
+  const timeRemaining = new Map();
+
+  const nextSteps = [...nodes]
+    .filter(([, node]) => node.parents.size === 0)
+    .map(([, node]) => node);
+
+  const active = new Set();
+
+  do {
+    nextSteps.sort(sort);
+
+    for (const [id, doneTime] of timeRemaining) {
+      if (doneTime === t) {
+        timeRemaining.delete(id);
+        active.delete(id);
+        elves += 1;
+
+        const job = nodes.get(id);
+        for (const child of job.children) {
+          child.parents.delete(job);
+          if (child.parents.size === 0) {
+            nextSteps.push(child);
+          }
+        }
+      }
+    }
+
+    const starting = nextSteps.splice(-elves);
+    elves -= starting.length;
+    for (const { id } of starting) {
+      timeRemaining.set(id, time.get(id) + t);
+      active.add(id);
+    }
+    if (active.size > 0) {
+      t = Math.min(...timeRemaining.values());
+    }
+  } while (active.size > 0);
+
+  return t;
 };
