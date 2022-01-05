5fdbae2e6b50acec1092ddc073fa402b56449174
Greg Walker <michael.walker@gsa.gov>
2022-01-05 13:15:57 -0600

    2018 day 8

--- 2018/README.md
+++ 2018/README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -111,16 +111,16 @@ Both parts: 3522.1752500000002ms
 
 ```
 Day 07
-Time part 1: 0.519ms
-Time part 2: 2.679ms
-Both parts: 3.197792ms
+Time part 1: 0.507ms
+Time part 2: 0.695ms
+Both parts: 1.202709ms
 ```
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 3.098ms
+Time part 2: 1.665ms
+Both parts: 4.763208ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 14/50
-Total time: 9867.793ms
+Total stars: 16/50
+Total time: 9870.561ms
 ```
 
 <!--/RESULTS-->
--- 2018/src/day08/day08.js
+++ 2018/src/day08/day08.js
@@ -1,11 +1,67 @@
-const input = (raw) => {};
+const input = (raw) => raw.trim().split(" ").map(Number);
+
+const getTree = (stream) => {
+  const nodes = [];
+  const active = [
+    {
+      childCount: stream[0],
+      metaCount: stream[1],
+      children: [],
+      metadata: [],
+      parent: null,
+    },
+  ];
+
+  for (let i = 2; i < stream.length; i += 1) {
+    const parent = active.pop();
+
+    if (parent.children.length === parent.childCount) {
+      parent.metadata = stream.slice(i, i + parent.metaCount);
+      i += parent.metaCount - 1;
+      nodes.push(parent);
+      // done with this node; don't add it back
+    } else {
+      const child = {
+        childCount: stream[i],
+        metaCount: stream[i + 1],
+        children: [],
+        metadata: [],
+        parent,
+      };
+      parent.children.push(child);
+      active.push(parent);
+      active.push(child);
+      i += 1;
+    }
+  }
+
+  return nodes;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const stream = input(raw);
+  const nodes = getTree(stream);
+
+  return nodes
+    .flatMap(({ metadata }) => metadata)
+    .reduce((sum, c) => sum + c, 0);
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const stream = input(raw);
+  const nodes = getTree(stream);
+
+  const nodeValue = (node) => {
+    if (node.children.length === 0) {
+      return node.metadata.reduce((sum, c) => sum + c, 0);
+    }
+
+    return node.metadata
+      .filter((v) => v <= node.children.length)
+      .reduce((sum, index) => sum + nodeValue(node.children[index - 1]), 0);
+  };
+
+  const root = nodes.find(({ parent }) => parent === null);
+
+  return nodeValue(root);
 };
