f0f978723222eda42cd249edc15c1f7084eacbee
Greg Walker <michael.walker@gsa.gov>
2022-01-06 10:46:12 -0600

    2018 day 14

--- 2018/src/day14/day14.js
+++ 2018/src/day14/day14.js
@@ -1,11 +1,96 @@
-const input = (raw) => {};
+import mn from "mnemonist";
+
+const LinkedList = mn.LinkedList;
+
+const input = (raw) => +raw;
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const recipesToTry = input(raw);
+  const list = LinkedList.from([3, 7]);
+
+  const elves = [list.head, list.tail];
+
+  while (list.size < recipesToTry + 10) {
+    const score = elves
+      .reduce((sum, { item }) => sum + item, 0)
+      .toString()
+      .split("");
+
+    for (const num of score) {
+      list.push(+num);
+    }
+
+    const shift1 = elves[0].item;
+    for (let i = 0; i <= shift1; i += 1) {
+      elves[0] = elves[0].next ?? list.head;
+    }
+
+    const shift2 = elves[1].item;
+    for (let i = 0; i <= shift2; i += 1) {
+      elves[1] = elves[1].next ?? list.head;
+    }
+  }
+
+  let node = list.head;
+  for (let i = 0; i < recipesToTry; i += 1) {
+    node = node.next ?? list.head;
+  }
+
+  const score = [];
+  for (let i = 0; i < 10; i += 1) {
+    score.push(node.item);
+    node = node.next;
+  }
+
+  return +score.join("");
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const recipesToFind = input(raw).toString();
+  const list = LinkedList.from([3, 7]);
+
+  const elves = [list.head, list.tail];
+
+  const targetSize = recipesToFind.toString().length;
+  let from = list.head;
+  let last = 0;
+
+  while (last !== recipesToFind) {
+    const score = elves
+      .reduce((sum, { item }) => sum + item, 0)
+      .toString()
+      .split("");
+
+    for (const num of score) {
+      list.push(+num);
+
+      if (list.size > targetSize) {
+        const lastFew = [];
+        let pointer = from;
+        for (let i = 0; i < targetSize; i += 1) {
+          lastFew.push(pointer.item);
+          pointer = pointer.next;
+        }
+        last = lastFew.join("");
+
+        from = from.next;
+
+        if (last === recipesToFind) {
+          break;
+        }
+      }
+    }
+
+    const shift1 = elves[0].item;
+    for (let i = 0; i <= shift1; i += 1) {
+      elves[0] = elves[0].next ?? list.head;
+    }
+
+    const shift2 = elves[1].item;
+    for (let i = 0; i <= shift2; i += 1) {
+      elves[1] = elves[1].next ?? list.head;
+    }
+  }
+
+  return list.size - targetSize - 1;
 };
