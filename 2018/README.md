c2e2c47e946dba93174615f5d3282a8ab9ea421e
Greg Walker <mgwalker@gmail.com>
2022-01-08 16:38:11 -0600

    2018 day 15, complete; whew

--- 2018/README.md
+++ 2018/README.md
@@ -25,7 +25,7 @@
 [![Day](https://badgen.net/badge/12/%E2%98%85%E2%98%85/green)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
 [![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
-![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/15/%E2%98%85%E2%98%85/green)](src/day15)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
@@ -160,16 +160,16 @@ Both parts: 17.179624999999998ms
 
 ```
 Day 14
-Time part 1: 97.587ms
-Time part 2: 4281.034ms
-Both parts: 4378.620583ms
+Time part 1: 95.78ms
+Time part 2: 4243.669ms
+Both parts: 4339.448125ms
 ```
 
 ```
 Day 15
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2815.458ms
+Time part 2: 57379.766ms
+Both parts: 60195.22354200001ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 28/50
-Total time: 15904.561ms
+Total stars: 30/50
+Total time: 76060.612ms
 ```
 
 <!--/RESULTS-->
--- 2018/src/day15/day15.js
+++ 2018/src/day15/day15.js
@@ -14,6 +14,7 @@ const input = (raw) => {
         units.push({
           x,
           y,
+          ap: 3,
           hp: 200,
           species: board[y][x] === "E" ? "elf" : "goblin",
         });
@@ -21,7 +22,7 @@ const input = (raw) => {
     }
   }
 
-  return { board, units };
+  return { board, units: new Set(units) };
 };
 
 const xySort = ([xa, ya], [xb, yb]) => {
@@ -31,160 +32,177 @@ const xySort = ([xa, ya], [xb, yb]) => {
   return ya - yb;
 };
 
+const attackMaybe = (unit, enemies, units) => {
+  const candidates = [
+    [unit.x, unit.y - 1],
+    [unit.x - 1, unit.y],
+    [unit.x + 1, unit.y],
+    [unit.x, unit.y + 1],
+  ]
+    .map(([x, y]) => enemies.get(`${x},${y}`))
+    .filter((v) => !!v)
+    // They're already in reading order, so now sort by HP order. If they tie
+    // at HP, they'll remain in reading order.
+    .sort(({ hp: a }, { hp: b }) => a - b);
+
+  if (candidates.length === 0) {
+    // There aren't any candidates to attack
+    return false;
+  }
+
+  const enemy = candidates[0];
+  enemy.hp -= unit.ap;
+
+  if (enemy.hp <= 0) {
+    // Oh no, enemy is kaput.
+    units.delete(enemy);
+  }
+  return true;
+};
+
 const unitSort = ({ x: xa, y: ya }, { x: xb, y: yb }) =>
   xySort([xa, ya], [xb, yb]);
+
+const unitTurn = (unit, units, room) => {
+  const enemies = new Map(
+    [...units]
+      .filter((u) => u !== unit && u.species !== unit.species)
+      .map((u) => [`${u.x},${u.y}`, u])
+  );
+  if (enemies.size === 0) {
+    // There aren't any enemies at all, so just bail out. There's nothing for
+    // this unit to do.
+    return;
+  }
+
+  const pathfinder = new PF.AStarFinder();
+
+  const board = room.clone();
+  for (const u of units) {
+    if (u !== unit) {
+      board.setWalkableAt(u.x, u.y, false);
+    }
+  }
+
+  if (!attackMaybe(unit, enemies, units)) {
+    const potentialSteps = [
+      [unit.x, unit.y - 1],
+      [unit.x - 1, unit.y],
+      [unit.x + 1, unit.y],
+      [unit.x, unit.y + 1],
+    ].filter(([x, y]) => board.nodes[y][x].walkable);
+
+    const potentialGoals = [...enemies.values()]
+      .map(({ x, y }) => [
+        [x, y - 1],
+        [x - 1, y],
+        [x + 1, y],
+        [x, y + 1],
+      ])
+      .flat();
+
+    const potentialPaths = potentialSteps
+      .map((step) =>
+        potentialGoals.map((goal) => ({
+          from: step,
+          to: goal,
+          path: pathfinder.findPath(
+            step[0],
+            step[1],
+            goal[0],
+            goal[1],
+            board.clone()
+          ),
+        }))
+      )
+      .flat()
+      .filter(({ path }) => path.length > 0);
+
+    if (potentialPaths.length === 0) {
+      return;
+    }
+
+    const shortestLength = Math.min(
+      ...potentialPaths.map(({ path }) => path.length)
+    );
+
+    const shortestPaths = potentialPaths
+      .filter(({ path }) => path.length === shortestLength)
+      .sort((a, b) => xySort(a.from, b.from))
+      .sort((a, b) => xySort(a.to, b.to));
+
+    const next = shortestPaths[0].from;
+    unit.x = next[0];
+    unit.y = next[1];
+
+    attackMaybe(unit, enemies, units);
+  }
+};
+
+const playRound = (units, room) => {
+  if (new Set([...units].map(({ species }) => species)).size === 1) {
+    // There's only one species left, so this game is over because there can't
+    // be anymore combat
+    return false;
+  }
+
+  const sortedUnits = [...units].sort(unitSort);
+  units.clear();
+  sortedUnits.forEach((u) => units.add(u));
+
+  for (const unit of units) {
+    unitTurn(unit, units, room);
+  }
+  return true;
+};
 
 export const part1 = (raw) => {
   const { board, units } = input(raw);
   const room = new PF.Grid(
     board.map((row) => row.map((v) => (v === "#" ? 1 : 0)))
   );
-  const pathfinder = new PF.BreadthFirstFinder();
 
-  const takeTurn = (unit) => {
-    if (unit.hp <= 0) {
-      return;
-    }
+  let round = 0;
+  while (playRound(units, room)) {
+    round += 1;
+  }
 
-    const enemy = unit.species === "elf" ? "goblin" : "elf";
-    const enemyID = enemy[0].toUpperCase();
-
-    if (!units.some(({ species }) => species === enemy)) {
-      // If there aren't any enemies left, this unit doesn't actually need to
-      // do anything, so bail out.
-      return;
-    }
-
-    const fightMaybe = () => {
-      const { x, y } = unit;
-
-      const enemiesToEvaluate = [
-        [x, y - 1],
-        [x - 1, y],
-        [x + 1, y],
-        [x, y + 1],
-      ]
-        .filter(([xx, yy]) => board[yy][xx] === enemyID)
-        .map(([xx, yy]) =>
-          units.find(({ x: xu, y: yu }) => xu === xx && yu === yy)
-        )
-        .filter((u) => !!u)
-        .sort(({ hp: a }, { hp: b }) => a - b);
-
-      if (enemiesToEvaluate.length > 0) {
-        const enemyToAttack = enemiesToEvaluate[0];
-
-        enemyToAttack.hp -= 3;
-
-        if (enemyToAttack.hp <= 0) {
-          units.splice(units.indexOf(enemyToAttack), 1);
-          board[enemyToAttack.y][enemyToAttack.x] = ".";
-        }
-        return true;
-      }
-      return false;
-    };
-
-    if (!fightMaybe()) {
-      const enemies = units.filter(({ species }) => species === enemy);
-
-      const constrainedRoom = room.clone();
-      for (const otherUnit of units) {
-        if (otherUnit !== unit) {
-          constrainedRoom.setWalkableAt(otherUnit.x, otherUnit.y, false);
-        }
-      }
-
-      const paths = enemies
-        .map(({ x, y }) => {
-          const clone = constrainedRoom.clone();
-          clone.setWalkableAt(x, y, true);
-          return [
-            [x, y - 1],
-            [x - 1, y],
-            [x + 1, y],
-            [x, y + 1],
-          ].map(([ex, ey]) =>
-            pathfinder.findPath(unit.x, unit.y, ex, ey, clone.clone())
-          );
-        })
-        .flat()
-        .filter((p) => p.length > 0);
-
-      if (paths.length > 0) {
-        const shortest = Math.min(
-          ...paths.map((p) => (p.length > 0 ? p.length : Infinity))
-        );
-
-        const potentialNextSteps = paths
-          .filter((p) => p.length === shortest)
-          .map((p) => p[1]);
-        const [newX, newY] = potentialNextSteps.sort(xySort)[0];
-
-        if (potentialNextSteps.length > 4) {
-          console.log(paths);
-          console.log(potentialNextSteps);
-          console.log(newX, newY);
-          console.log("============");
-
-          // throw new Error();
-        }
-
-        board[unit.y][unit.x] = ".";
-        board[newY][newX] = unit.species[0].toUpperCase();
-
-        unit.x = newX;
-        unit.y = newY;
-
-        fightMaybe();
-      }
-    }
-  };
-
-  let rounds = 0;
-
-  const print = () => {
-    console.log();
-    console.log(`===== After ${rounds} rounds =====`);
-    for (let y = 0; y < board.length; y += 1) {
-      console.log(
-        board[y].join(""),
-        "  ",
-        units
-          .filter(({ y: yy }) => y === yy)
-          .sort(({ x: xa }, { x: xb }) => xa - xb)
-          .map(({ hp, species }) => `${species[0]}(${hp})`)
-          .join(", ")
-      );
-    }
-  };
-
-  print();
-  const round = () => {
-    units.sort(unitSort);
-
-    if (new Set(units.map(({ species }) => species)).size === 1) {
-      return false;
-    }
-    rounds += 1;
-
-    for (const unit of [...units]) {
-      takeTurn(unit);
-    }
-
-    // if (rounds === 1) {
-    print();
-    // }
-
-    return true;
-  };
-
-  while (round()) {}
-
-  return (rounds - 1) * units.reduce((sum, { hp }) => sum + hp, 0);
+  return (round - 1) * [...units].reduce((sum, { hp }) => sum + hp, 0);
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const { board, units } = input(raw);
+  const room = new PF.Grid(
+    board.map((row) => row.map((v) => (v === "#" ? 1 : 0)))
+  );
+
+  const elves = [...units].filter(({ species }) => species === "elf").length;
+
+  let elfAP = 3;
+  let gameUnits;
+  let round;
+  let survivors;
+
+  do {
+    elfAP += 1;
+    round = 0;
+    gameUnits = new Set(JSON.parse(JSON.stringify([...units])));
+
+    [...gameUnits]
+      .filter(({ species }) => species === "elf")
+      // eslint-disable-next-line no-loop-func
+      .forEach((elf) => {
+        elf.ap = elfAP;
+      });
+
+    while (playRound(gameUnits, room)) {
+      round += 1;
+    }
+
+    survivors = [...gameUnits].filter(
+      ({ species }) => species === "elf"
+    ).length;
+  } while (survivors !== elves);
+
+  return (round - 1) * [...gameUnits].reduce((sum, { hp }) => sum + hp, 0);
 };
