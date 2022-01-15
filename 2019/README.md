593f113294d83257f590061393055e3d83c399b3
Greg Walker <mgwalker@gmail.com>
2022-01-15 16:57:38 -0600

    2019 day 8

--- 2019/README.md
+++ 2019/README.md
@@ -18,7 +18,7 @@
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 [![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
-![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
@@ -118,9 +118,9 @@ Both parts: 28.390792ms
 
 ```
 Day 08
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 20.952ms
+Time part 2: 19.94ms
+Both parts: 40.891915999999995ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 14/50
-Total time: 496.119ms
+Total stars: 16/50
+Total time: 537.011ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day08/day08.js
+++ 2019/src/day08/day08.js
@@ -1,11 +1,57 @@
-const input = (raw) => {};
+const input = (raw) => {
+  const columns = 25;
+  const rows = 6;
+
+  const layers = [];
+  let offset = 0;
+
+  while (offset < raw.length) {
+    const pixels = [];
+    const flatPixels = raw.trim().split("").map(Number);
+
+    for (let i = 0; i < rows; i += 1) {
+      const row = flatPixels.slice(
+        i * columns + offset,
+        (i + 1) * columns + offset,
+      );
+      pixels.push(row);
+    }
+
+    offset += columns * rows;
+    layers.push(pixels);
+  }
+
+  return layers;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const layers = input(raw).map((layer) => layer.flat());
+
+  const zeroes = layers
+    .map((layer) => [layer.filter((v) => v === 0).length, layer])
+    .sort(([a], [b]) => a - b);
+
+  const target = zeroes[0][1];
+  const ones = target.filter((v) => v === 1).length;
+  const twos = target.filter((v) => v === 2).length;
+
+  return ones * twos;
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const layers = input(raw);
+
+  const image = [];
+  for (let y = 0; y < layers[0].length; y += 1) {
+    const row = [];
+    for (let x = 0; x < layers[0][0].length; x += 1) {
+      const p = layers.map((l) => l[y][x]).filter((v) => v !== 2);
+      row.push(p[0]);
+    }
+    image.push(row);
+  }
+
+  return image
+    .map((row) => row.map((v) => (v === 0 ? " " : "█")).join(""))
+    .join("\n");
 };
