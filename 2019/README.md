495b655b7153b271a4ec92e9422f6b65a6fd1734
Greg Walker <mgwalker@gmail.com>
2022-01-15 14:30:06 -0600

    2019 day 6

--- 2019/README.md
+++ 2019/README.md
@@ -16,7 +16,7 @@
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 [![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
-![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/06/%E2%98%85%E2%98%85/green)](src/day06)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/09/%E2%98%86%E2%98%86/gray)
@@ -104,9 +104,9 @@ Both parts: 0.5149159999999999ms
 
 ```
 Day 06
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 2.94ms
+Time part 2: 1.309ms
+Both parts: 4.2489989999999995ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 10/50
-Total time: 463.479ms
+Total stars: 12/50
+Total time: 467.728ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day06/day06.js
+++ 2019/src/day06/day06.js
@@ -1,11 +1,108 @@
-const input = (raw) => {};
+class Body {
+  #_children;
+  #_name;
+  #_parent;
+
+  constructor(name) {
+    this.#_name = name;
+    this.#_children = [];
+  }
+
+  get name() {
+    return this.#_name;
+  }
+
+  set parent(p) {
+    this.#_parent = p;
+  }
+
+  get parent() {
+    return this.#_parent;
+  }
+
+  push(child) {
+    this.#_children.push(child);
+  }
+
+  get children() {
+    return this.#_children;
+  }
+
+  get orbits() {
+    let p = this.#_parent;
+    let orbits = 0;
+    while (p) {
+      orbits += 1;
+      p = p.parent;
+    }
+    return orbits;
+  }
+}
+
+const input = (raw) => {
+  const bodies = new Map();
+  const hosts = raw.split("\n").map((v) => v.split(")"));
+  for (const [host, satellite] of hosts) {
+    const hostBody =
+      bodies.get(host) ?? bodies.set(host, new Body(host)).get(host);
+    const satelliteBody =
+      bodies.get(satellite) ??
+      bodies.set(satellite, new Body(satellite)).get(satellite);
+
+    hostBody.push(satelliteBody);
+    satelliteBody.parent = hostBody;
+  }
+
+  return bodies;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const bodies = input(raw);
+  return [...bodies.values()].reduce((sum, b) => sum + b.orbits, 0);
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const bodies = input(raw);
+
+  const me = bodies.get("YOU");
+  const he = bodies.get("SAN");
+
+  const myOrbits = new Set();
+  let parent = me.parent;
+  while (parent) {
+    myOrbits.add(parent);
+    parent = parent.parent;
+  }
+
+  let common;
+  parent = he.parent;
+  while (parent) {
+    if (myOrbits.has(parent)) {
+      common = parent;
+      break;
+    }
+    parent = parent.parent;
+  }
+
+  let meDepth = 0;
+  parent = me.parent;
+  while (parent) {
+    if (parent === common) {
+      break;
+    }
+    parent = parent.parent;
+    meDepth += 1;
+  }
+
+  let heDepth = 0;
+  parent = he.parent;
+  while (parent) {
+    if (parent === common) {
+      break;
+    }
+    parent = parent.parent;
+    heDepth += 1;
+  }
+
+  return meDepth + heDepth;
 };
