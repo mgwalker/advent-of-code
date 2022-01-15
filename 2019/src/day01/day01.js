b1e3605e4f4ab3093d425d919b793ba953659d4d
Greg Walker <mgwalker@gmail.com>
2022-01-14 20:20:03 -0600

    2019 day 1

--- 2019/src/day01/day01.js
+++ 2019/src/day01/day01.js
@@ -1,11 +1,19 @@
-const input = (raw) => {};
+const input = (raw) => raw.trim().split("\n").map(Number);
 
-export const part1 = (raw) => {
-  const data = input(raw);
-  return;
-};
+export const part1 = (raw) =>
+  input(raw)
+    .map((mass) => Math.floor(mass / 3) - 2)
+    .reduce((sum, mass) => sum + mass, 0);
 
-export const part2 = (raw) => {
-  const data = input(raw);
-  return;
-};
+export const part2 = (raw) =>
+  input(raw)
+    .map((mass) => {
+      let totalMass = 0;
+      let newFuel = Math.floor(mass / 3) - 2;
+      while (newFuel > 0) {
+        totalMass += newFuel;
+        newFuel = Math.floor(newFuel / 3) - 2;
+      }
+      return totalMass;
+    })
+    .reduce((sum, mass) => sum + mass, 0);
