7cf0de009f12cfb520c5b9b1a859fad547867fb6
Greg Walker <mgwalker@gmail.com>
2022-01-15 20:40:00 -0600

    2019 day 10

--- 2019/README.md
+++ 2019/README.md
@@ -20,7 +20,7 @@
 [![Day](https://badgen.net/badge/07/%E2%98%85%E2%98%85/green)](src/day07)
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
-![Day](https://badgen.net/badge/10/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
 ![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
@@ -132,9 +132,9 @@ Both parts: 331.27900100000005ms
 
 ```
 Day 10
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 30.731ms
+Time part 2: 79.516ms
+Both parts: 110.24725ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 18/50
-Total time: 868.29ms
+Total stars: 20/50
+Total time: 978.537ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day10/day10.js
+++ 2019/src/day10/day10.js
@@ -1,11 +1,115 @@
-const input = (raw) => {};
+import DefaultMap from "../utils/DefaultMap.js";
+
+function* rangeExclusive(a, b) {
+  const step = a < b ? 1 : -1;
+
+  let x = a + step;
+  while (x !== b) {
+    yield x;
+    x += step;
+  }
+}
+
+const input = (raw) =>
+  raw
+    .trim()
+    .split("\n")
+    .map((v) => v.split(""));
+
+const getWhichAsteroidsCanSeeWhichOthers = (space) => {
+  const canSee = new DefaultMap(() => new Set());
+
+  const asteroids = space
+    .map((row, y) =>
+      row
+        .map((object, x) => (object === "#" ? [x, y] : false))
+        .filter((v) => !!v),
+    )
+    .flat();
+
+  for (let i = 0; i < asteroids.length; i += 1) {
+    const [xa, ya] = asteroids[i];
+    const asteroid = asteroids[i].join(",");
+    const peers = canSee.get(asteroid);
+
+    for (let j = i + 1; j < asteroids.length; j += 1) {
+      const [xb, yb] = asteroids[j];
+      const potential = asteroids[j].join(",");
+
+      if (xa === xb) {
+        let obstructed = false;
+        for (const y of rangeExclusive(ya, yb)) {
+          if (space[y][xa] === "#") {
+            obstructed = true;
+          }
+        }
+        if (!obstructed) {
+          peers.add(potential);
+          canSee.get(potential).add(asteroid);
+        }
+      } else {
+        const m = (yb - ya) / (xb - xa);
+        const b = ya - m * xa;
+
+        let obstructed = false;
+        for (const x of rangeExclusive(xa, xb)) {
+          // Handle floating-point glitches
+          const y = Math.round((m * x + b) * 1000) / 1000;
+
+          if (y === Math.floor(y) && space[y][x] === "#") {
+            obstructed = true;
+          }
+        }
+        if (!obstructed) {
+          peers.add(potential);
+          canSee.get(potential).add(asteroid);
+        }
+      }
+    }
+  }
+
+  return canSee;
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const space = input(raw);
+  const canSee = getWhichAsteroidsCanSeeWhichOthers(space);
+
+  return Math.max(...[...canSee.values()].map((v) => v.size));
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+  const space = input(raw);
+  const canSee = getWhichAsteroidsCanSeeWhichOthers(space);
+
+  const most = Math.max(...[...canSee.values()].map((v) => v.size));
+  const [x, y] = [...canSee.keys()]
+    .find((asteroid) => canSee.get(asteroid).size === most)
+    .split(",")
+    .map(Number);
+
+  const thereAreAsteroids = () =>
+    space.flat().filter((v) => v === "#").length > 1;
+
+  const asploded = [false];
+
+  // For funsies, blow them all up. We only *need* the first 200, but just go
+  // to town and destroy them all.
+  while (thereAreAsteroids()) {
+    const canSeeNow = getWhichAsteroidsCanSeeWhichOthers(space);
+
+    const targets = [...canSeeNow.get(`${x},${y}`)]
+      .map((v) => v.split(",").map(Number))
+      .map(([xx, yy]) => [xx, yy, (Math.atan2(xx - x, y - yy) * 180) / Math.PI])
+      .map(([xx, yy, angle]) => [xx, yy, angle >= 0 ? angle : 360 + angle])
+      .sort(([, , a], [, , b]) => a - b);
+
+    for (const asteroid of targets) {
+      const [xx, yy] = asteroid;
+      asploded.push([xx, yy]);
+      space[yy][xx] = ".";
+    }
+  }
+
+  return asploded[200][0] * 100 + asploded[200][1];
 };
