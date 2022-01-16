e7d11dac5fa9b4bb7a298cf39ecaa1af856d7f27
Greg Walker <mgwalker@gmail.com>
2022-01-15 21:17:38 -0600

    2019 day 11

--- 2019/README.md
+++ 2019/README.md
@@ -21,7 +21,7 @@
 [![Day](https://badgen.net/badge/08/%E2%98%85%E2%98%85/green)](src/day08)
 [![Day](https://badgen.net/badge/09/%E2%98%85%E2%98%85/green)](src/day09)
 [![Day](https://badgen.net/badge/10/%E2%98%85%E2%98%85/green)](src/day10)
-![Day](https://badgen.net/badge/11/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/13/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
@@ -132,16 +132,16 @@ Both parts: 331.27900100000005ms
 
 ```
 Day 10
-Time part 1: 30.731ms
-Time part 2: 79.516ms
-Both parts: 110.24725ms
+Time part 1: 31.549ms
+Time part 2: 77.735ms
+Both parts: 109.284ms
 ```
 
 ```
 Day 11
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 67.719ms
+Time part 2: 10.011ms
+Both parts: 77.729416ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 20/50
-Total time: 978.537ms
+Total stars: 22/50
+Total time: 1055.303ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day11/day11.js
+++ 2019/src/day11/day11.js
@@ -1,11 +1,93 @@
-const input = (raw) => {};
+import DefaultMap from "../utils/DefaultMap.js";
+import intcode, { InputQueue } from "./Intcode.js";
 
-export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+const input = (raw) => raw.trim().split(",").map(Number);
+
+const paint = async (ops, startColor = 0n) => {
+  const panels = new DefaultMap(0n);
+  panels.set("0,0", startColor);
+
+  const position = [0, 0];
+  const delta = [0, -1];
+
+  const queue = new InputQueue();
+
+  const paintPanel = (color) => {
+    panels.set(`${position[0]},${position[1]}`, color);
+  };
+
+  const turn = (direction) => {
+    if (direction === 0n) {
+      // turn left 90 degrees
+      if (delta[1] === -1) {
+        delta[0] = -1;
+        delta[1] = 0;
+      } else if (delta[1] === 1) {
+        delta[0] = 1;
+        delta[1] = 0;
+      } else if (delta[0] === 1) {
+        delta[0] = 0;
+        delta[1] = -1;
+      } else {
+        delta[0] = 0;
+        delta[1] = 1;
+      }
+    } else if (direction === 1n) {
+      // turn right 90 degrees
+      if (delta[1] === -1) {
+        delta[0] = 1;
+        delta[1] = 0;
+      } else if (delta[1] === 1) {
+        delta[0] = -1;
+        delta[1] = 0;
+      } else if (delta[0] === 1) {
+        delta[0] = 0;
+        delta[1] = 1;
+      } else {
+        delta[0] = 0;
+        delta[1] = -1;
+      }
+    }
+
+    position[0] += delta[0];
+    position[1] += delta[1];
+    queue.put(panels.get(`${position[0]},${position[1]}`));
+  };
+
+  let action = 0;
+  const actions = [paintPanel, turn];
+
+  const output = (v) => {
+    actions[action % 2](v);
+    action += 1;
+  };
+
+  queue.put(panels.get(`${position[0]},${position[1]}`));
+  await intcode(ops, queue, output);
+
+  return panels;
 };
 
-export const part2 = (raw) => {
-  const data = input(raw);
-  return;
+export const part1 = async (raw) => {
+  const ops = input(raw);
+  const panels = await paint(ops);
+
+  return panels.size;
+};
+
+export const part2 = async (raw) => {
+  const ops = input(raw);
+  const panels = await paint(ops, 1n);
+
+  const maxX = Math.max(
+    ...[...panels.keys()].map((v) => v.split(",").map(Number)).map(([x]) => x),
+  );
+
+  const image = [...Array(6)].map(() => [...Array(maxX)].map(() => " "));
+  for (const [coords, color] of panels) {
+    const [x, y] = coords.split(",").map(Number);
+    image[y][x] = color === 1n ? "█" : " ";
+  }
+
+  return image.map((row) => row.join("")).join("\n");
 };
