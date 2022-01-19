75f8b18284774512715039a05860be0b0a9f9460
Greg Walker <michael.walker@gsa.gov>
2022-01-19 16:47:49 -0600

    2019 day 14 part 1

--- 2019/README.md
+++ 2019/README.md
@@ -24,7 +24,7 @@
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 ![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
-![Day](https://badgen.net/badge/14/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/yellow)](src/day14)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
@@ -153,16 +153,16 @@ Both parts: -
 
 ```
 Day 13
-Time part 1: 21.669ms
-Time part 2: 12166.615ms
-Both parts: 12188.283625ms
+Time part 1: 22.861ms
+Time part 2: 408.805ms
+Both parts: 431.666001ms
 ```
 
 ```
 Day 14
-Time part 1: -
+Time part 1: 0.728ms
 Time part 2: -
-Both parts: -
+Both parts: 0.728333ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 24/50
-Total time: 13243.587ms
+Total stars: 25/50
+Total time: 1487.698ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day14/day14.js
+++ 2019/src/day14/day14.js
@@ -1,11 +1,80 @@
-const input = (raw) => {};
+import DefaultMap from "../utils/DefaultMap.js";
+
+class Chemical {
+  #_chemical;
+  #_inputs;
+  #_perReaction;
+
+  static #_chemicals = new Map();
+  static #_storage = new DefaultMap(0);
+  static #_created = new DefaultMap(0);
+
+  constructor(reaction) {
+    const [inputs, output] = reaction.split(" => ");
+    const [, outCount, outChemical] = output.match(/(\d+) (.+)/);
+
+    this.#_chemical = outChemical;
+    this.#_perReaction = outCount;
+
+    this.#_inputs = [];
+    for (const reactionInput of inputs.split(", ")) {
+      const [, inCount, inChemical] = reactionInput.match(/(\d+) (.+)/) ?? [];
+      if (inChemical) {
+        this.#_inputs.push({ chemical: inChemical, count: +inCount });
+      }
+    }
+
+    Chemical.#_chemicals.set(outChemical, this);
+  }
+
+  static get(chemical) {
+    return Chemical.#_chemicals.get(chemical);
+  }
+
+  static howMuch(chemical) {
+    return Chemical.#_created.get(chemical);
+  }
+
+  make(count) {
+    const inStorage = Chemical.#_storage.get(this.#_chemical);
+    const toMake =
+      inStorage >= count
+        ? 0
+        : Math.ceil((count - inStorage) / this.#_perReaction) *
+          this.#_perReaction;
+    const leftover = Math.max(0, inStorage + toMake - count);
+
+    if (toMake > 0) {
+      for (const { chemical, count } of this.#_inputs) {
+        Chemical.get(chemical).make((count * toMake) / this.#_perReaction);
+      }
+
+      Chemical.#_created.set(
+        this.#_chemical,
+        Chemical.#_created.get(this.#_chemical) + toMake,
+      );
+    }
+
+    Chemical.#_storage.set(this.#_chemical, leftover);
+  }
+}
+
+const input = (raw) =>
+  [
+    raw
+      .trim()
+      .split("\n")
+      .map((s) => new Chemical(s)),
+    new Chemical(" => 1 ORE"),
+  ].flat();
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  input(raw);
+  Chemical.get("FUEL").make(1);
+  return Chemical.howMuch("ORE");
 };
 
 export const part2 = (raw) => {
-  const data = input(raw);
+  input(raw);
   return;
 };
