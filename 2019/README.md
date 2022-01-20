a38f4f845e219b9ca9daa1e30b300aeb9c38be94
Greg Walker <michael.walker@gsa.gov>
2022-01-20 13:37:39 -0600

    2019 day 16, part 1

--- 2019/README.md
+++ 2019/README.md
@@ -24,9 +24,9 @@
 [![Day](https://badgen.net/badge/11/%E2%98%85%E2%98%85/green)](src/day11)
 [![Day](https://badgen.net/badge/12/%E2%98%86%E2%98%86/yellow)](src/day12)
 [![Day](https://badgen.net/badge/13/%E2%98%85%E2%98%85/green)](src/day13)
-[![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%86/green)](src/day14)
+[![Day](https://badgen.net/badge/14/%E2%98%85%E2%98%85/green)](src/day14)
 ![Day](https://badgen.net/badge/15/%E2%98%86%E2%98%86/gray)
-![Day](https://badgen.net/badge/16/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/16/%E2%98%85%E2%98%86/yellow)](src/day16)
 ![Day](https://badgen.net/badge/17/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/18/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/19/%E2%98%86%E2%98%86/gray)
@@ -160,9 +160,9 @@ Both parts: 431.666001ms
 
 ```
 Day 14
-Time part 1: 0.728ms
-Time part 2: 60837.498875ms
-Both parts: 0.728333ms
+Time part 1: 0.608ms
+Time part 2: 59116.025ms
+Both parts: 59116.632708000005ms
 ```
 
 ```
@@ -174,9 +174,9 @@ Both parts: -
 
 ```
 Day 16
-Time part 1: -
+Time part 1: 701.794ms
 Time part 2: -
-Both parts: -
+Both parts: 701.794334ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 25/50
-Total time: 1487.698ms
+Total stars: 27/50
+Total time: 61305.396ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day16/day16.js
+++ 2019/src/day16/day16.js
@@ -1,11 +1,86 @@
-const input = (raw) => {};
+const input = (raw) => raw.trim();
+
+function* pattern(repeats = 1) {
+  const p = [0, 1, 0, -1];
+  let i = 0;
+  let r = 0;
+
+  if (repeats === 1) {
+    i = 1;
+  } else {
+    r = 1;
+  }
+
+  do {
+    yield p[i % p.length];
+
+    r += 1;
+    if (r >= repeats) {
+      r = 0;
+      i += 1;
+    }
+  } while (true);
+}
 
 export const part1 = (raw) => {
   const data = input(raw);
-  return;
+
+  const process = (num) => {
+    const digits = num.split("").map(Number);
+
+    return digits
+      .map((_, i) => {
+        const p = pattern(i + 1);
+        let sum = 0;
+        for (const d of digits) {
+          const pp = p.next().value;
+          sum += d * pp;
+        }
+        return Math.abs(sum) % 10;
+      })
+      .join("")
+      .padStart(digits.length, "0");
+  };
+
+  let out = data;
+  for (let i = 0; i < 100; i += 1) {
+    out = process(out);
+  }
+
+  return out.substr(0, 8);
 };
 
 export const part2 = (raw) => {
   const data = input(raw);
-  return;
+
+  const process = (num) => {
+    const digits = num.split("").map(Number);
+
+    return digits
+      .map((_, i) => {
+        const p = pattern(i + 1);
+        let sum = 0;
+        for (const d of digits) {
+          const pp = p.next().value;
+          sum += d * pp;
+        }
+        return Math.abs(sum) % 10;
+      })
+      .join("")
+      .padStart(digits.length, "0");
+  };
+
+  const len = +data.substring(0, 7);
+
+  let out = [...Array(10_000)]
+    .flatMap(() => data)
+    .join("")
+    .substring(0, len);
+
+  for (let i = 0; i < 100; i += 1) {
+    console.log(i);
+    out = process(out);
+  }
+
+  return out.substr(0, 8);
 };
