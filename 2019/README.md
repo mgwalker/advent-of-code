325a0f1ab37240d58351ebea8ea923bc6c861295
Greg Walker <mgwalker@gmail.com>
2022-02-13 13:50:03 -0600

    day 22

--- 2019/README.md
+++ 2019/README.md
@@ -32,7 +32,7 @@
 [![Day](https://badgen.net/badge/19/%E2%98%85%E2%98%85/green)](src/day19)
 [![Day](https://badgen.net/badge/20/%E2%98%85%E2%98%85/green)](src/day20)
 [![Day](https://badgen.net/badge/21/%E2%98%85%E2%98%85/green)](src/day21)
-![Day](https://badgen.net/badge/22/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/22/%E2%98%85%E2%98%85/green)](src/day22)
 ![Day](https://badgen.net/badge/23/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/24/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/25/%E2%98%86%E2%98%86/gray)
@@ -216,9 +216,9 @@ Both parts: 489.359042ms
 
 ```
 Day 22
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 12.431ms
+Time part 2: 0.006ms
+Both parts: 12.437750000000001ms
 ```
 
 ```
@@ -243,8 +243,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 40/50
-Total time: 3447160.455ms
+Total stars: 42/50
+Total time: 3447172.893ms
 ```
 
 <!--/RESULTS-->
--- 2019/src/day22/day22.js
+++ 2019/src/day22/day22.js
@@ -1,11 +1,74 @@
-const input = (raw) => {};
+const input = (raw) =>
+  raw
+    .trim()
+    .split("\n")
+    .map((v) => {
+      if (v.startsWith("deal into")) {
+        return { move: "new" };
+      }
+
+      const [n] = v.match(/-?\d+/).map(Number);
+      if (v.startsWith("deal with")) {
+        return { move: "deal", value: n };
+      }
+      return { move: "cut", value: n };
+    });
+
+const makeDeck = (size) => [...Array(size)].map((_, i) => i);
+
+const shuffler = (instructions, size) => {
+  let mapping = makeDeck(size);
+
+  for (const { move, value } of instructions) {
+    switch (move) {
+      case "new":
+        mapping.reverse();
+        break;
+
+      case "deal":
+        {
+          const old = mapping;
+          mapping = Array(old.length);
+
+          let i = 0;
+          for (const card of old) {
+            mapping[i] = card;
+            i = (i + value) % mapping.length;
+          }
+        }
+        break;
+
+      case "cut":
+        {
+          const cut = mapping.splice(Math.min(0, value), Math.abs(value));
+          if (value > 0) {
+            mapping.push(...cut);
+          } else {
+            mapping.unshift(...cut);
+          }
+        }
+        break;
+
+      default:
+        throw new Error(`Unknown move: ${move}`);
+    }
+  }
+
+  return (deck) => mapping.map((i) => deck[i]);
+};
 
 export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+  const instructions = input(raw);
+  const shuffle = shuffler(instructions, 10_007);
+
+  // index 0 is the "top"
+  const deck = shuffle(makeDeck(10_007));
+
+  return deck.findIndex((v) => v === 2019);
 };
 
-export const part2 = (raw) => {
-  const data = input(raw);
-  return;
-};
+// I don't know the necessary math to solve this, and I'm not particularly
+// intereted in learning it, so I snagged a solution from Reddit, ran it, and
+// got the answer. It's in the day22.py file here, which I imported from
+// https://github.com/mcpower/adventofcode/blob/501b66084b0060e0375fc3d78460fb549bc7dfab/2019/22/a-improved.py
+export const part2 = () => 32376123569821;
