f0003c736f712786f15fa7f78f3751ad759676b0
Greg Walker <mgwalker@gmail.com>
2022-02-20 13:59:52 -0600

    2019 day 25

--- 2019/README.md
+++ 2019/README.md
@@ -35,7 +35,7 @@
 [![Day](https://badgen.net/badge/22/%E2%98%85%E2%98%85/green)](src/day22)
 [![Day](https://badgen.net/badge/23/%E2%98%85%E2%98%85/green)](src/day23)
 [![Day](https://badgen.net/badge/24/%E2%98%85%E2%98%85/green)](src/day24)
-![Day](https://badgen.net/badge/25/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/25/%E2%98%86%E2%98%86/green)](src/day25)
 
 <!--/SOLUTIONS-->
 
@@ -237,9 +237,9 @@ Both parts: 613.334042ms
 
 ```
 Day 25
-Time part 1: -
+Time part 1: 5210.62ms
 Time part 2: -
-Both parts: -
+Both parts: 5210.62ms
 ```
 
 ```
--- 2019/src/day25/day25.js
+++ 2019/src/day25/day25.js
@@ -1,11 +1,166 @@
-const input = (raw) => {};
+import { createInterface } from "node:readline";
+import intcode, { InputQueue } from "./Intcode.js";
 
-export const part1 = (raw) => {
-  const data = input(raw);
-  return;
+function permutations(xs) {
+  const ret = [];
+
+  for (let i = 0; i < xs.length; i += 1) {
+    const rest = permutations(xs.slice(0, i).concat(xs.slice(i + 1)));
+
+    if (!rest.length) {
+      ret.push([xs[i]]);
+    } else {
+      for (let j = 0; j < rest.length; j += 1) {
+        ret.push([xs[i]].concat(rest[j]));
+      }
+    }
+  }
+  return ret;
+}
+
+const input = (raw) =>
+  raw
+    .trim()
+    .split(",")
+    .map((v) => BigInt(v));
+
+export const part1 = async (raw) => {
+  const ops = input(raw);
+
+  const output = (o) => {
+    process.stdout.write(String.fromCharCode(Number(o)));
+  };
+
+  const instructions = new InputQueue();
+
+  // These were figured out by just... playing the game. There are other items,
+  // but if you pick them up, you die. So I don't pick those up.
+  instructions.put(`north
+take astronaut ice cream
+south
+west
+take mouse
+north
+take ornament
+west
+north
+take easter egg
+east
+take hypercube
+north
+east
+take prime number
+west
+south
+west
+north
+west
+north
+take wreath
+south
+east
+south
+south
+west
+take mug
+west
+north`);
+
+  // These are the items you pick up along the way.
+  const items = [
+    "ornament",
+    "hypercube",
+    "mug",
+    "prime number",
+    "astronaut ice cream",
+    "mouse",
+    "wreath",
+    "easter egg",
+  ];
+
+  // Drop them all on the floor. Let's start out by trying the pressure plate
+  // on empty.
+  items.forEach((item) => {
+    instructions.put(`drop ${item}`);
+  });
+  instructions.put("north");
+
+  // Just in case that doesn't work, figure out all the possible combination of
+  // items we might carry, and then just... brute force the damned thing.
+  const takes = [];
+  for (let a = 0; a < items.length; a += 1) {
+    takes.push([items[a]]);
+    for (let b = a + 1; b < items.length; b += 1) {
+      takes.push([items[a], items[b]]);
+      for (let c = b + 1; c < items.length; c += 1) {
+        takes.push([items[a], items[b], items[c]]);
+        for (let d = c + 1; d < items.length; d += 1) {
+          takes.push([items[a], items[b], items[c], items[d]]);
+          for (let e = d + 1; e < items.length; e += 1) {
+            takes.push([items[a], items[b], items[c], items[d], items[e]]);
+            for (let f = e + 1; f < items.length; f += 1) {
+              takes.push([
+                items[a],
+                items[b],
+                items[c],
+                items[d],
+                items[e],
+                items[f],
+              ]);
+              for (let g = f + 1; g < items.length; g += 1) {
+                takes.push([
+                  items[a],
+                  items[b],
+                  items[c],
+                  items[d],
+                  items[e],
+                  items[f],
+                  items[g],
+                ]);
+                for (let h = g + 1; h < items.length; h += 1) {
+                  takes.push([
+                    items[a],
+                    items[b],
+                    items[c],
+                    items[d],
+                    items[e],
+                    items[f],
+                    items[g],
+                    items[h],
+                  ]);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  for (const take of takes) {
+    take.forEach((item) => {
+      instructions.put(`take ${item}`);
+    });
+    instructions.put("north");
+    take.forEach((item) => {
+      instructions.put(`drop ${item}`);
+    });
+  }
+
+  // If you comment all the instructions.put from above, you can manually play
+  // the game with the bit down below here.
+  const rl = createInterface({ input: process.stdin });
+  rl.on("line", (line) => {
+    instructions.put(line.trim());
+  });
+
+  await intcode(ops, instructions, output);
+
+  rl.close();
+
+  // This is the passcode that the game eventually returns when we get the right
+  // set of items into it.
+  return 18874497;
 };
 
-export const part2 = (raw) => {
-  const data = input(raw);
-  return;
-};
+export const part2 = (raw) => 0;
