424fb09165bcf12149109e9e249730815155b974
Greg Walker <mgwalker@gmail.com>
2022-12-04 23:22:41 -0600

    2022 day 5

--- 2022/README.md
+++ 2022/README.md
@@ -10,7 +10,7 @@
 [![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
 [![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
-![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/05/%E2%98%85%E2%98%85/green)](src/day05)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/08/%E2%98%86%E2%98%86/gray)
@@ -65,16 +65,16 @@ Both parts: 15.26325ms
 
 ```
 Day 04
-Time part 1: 1.647ms
-Time part 2: 1.415ms
-Both parts: 3.061832ms
+Time part 1: 1.499ms
+Time part 2: 1.426ms
+Both parts: 2.925333ms
 ```
 
 ```
 Day 05
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 1.192ms
+Time part 2: 0.471ms
+Both parts: 1.6628340000000001ms
 ```
 
 ```
@@ -218,8 +218,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 8/50
-Total time: 21.962ms
+Total stars: 10/50
+Total time: 23.488ms
 ```
 
 <!--/RESULTS-->
---
+++ 2022/src/day05/index.js
@@ -0,0 +1,60 @@
+import run from "aocrunner";
+import { sum, toNumbers } from "utils";
+
+const input = (raw) => {
+  const [stacksMeta, moves] = raw.split("\n\n");
+
+  const stackRows = stacksMeta.split("\n");
+  const count = stackRows.pop().match(/\d+/g).length;
+
+  const stacks = new Map([...Array(count)].map((_, i) => [i + 1, []]));
+
+  for (let level = 0; level < stackRows.length; level += 1) {
+    const boxes = stackRows[level]
+      .match(/(^|\s).{3}/g)
+      .map((v) => v.replace(/[\s\[\]]/g, ""));
+
+    for (let i = 0; i < boxes.length; i += 1) {
+      if (boxes[i].length > 0) {
+        stacks.get(i + 1).push(boxes[i]);
+      }
+    }
+  }
+
+  return {
+    stacks,
+    moves: moves.split("\n").map((v) => v.match(/(\d+)/g).map(toNumbers)),
+  };
+};
+
+export const part1 = (raw) => {
+  const { stacks, moves } = input(raw);
+
+  for (const move of moves) {
+    const [count, from, to] = move;
+
+    const movers = stacks.get(from).splice(0, count);
+    stacks.get(to).unshift(...movers.reverse());
+  }
+
+  return [...stacks].map(([, c]) => c[0]).join("");
+};
+
+export const part2 = (raw) => {
+  const { stacks, moves } = input(raw);
+
+  for (const move of moves) {
+    const [count, from, to] = move;
+
+    const movers = stacks.get(from).splice(0, count);
+    stacks.get(to).unshift(...movers);
+  }
+
+  return [...stacks].map(([, c]) => c[0]).join("");
+};
+
+run({
+  part1: { solution: part1 },
+  part2: { solution: part2 },
+  trimTestInputs: true,
+});
